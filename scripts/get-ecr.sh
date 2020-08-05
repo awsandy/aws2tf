5b6cc34bbf8ab491180a63b94560b9f51ba9f0a2
awsandy <awsandy@amazon.co.uk>
2020-08-05 16:11:42 +0100

    ecr

--- scripts/get-ecr.sh
+++ scripts/get-ecr.sh
@@ -23,21 +23,24 @@ for c in `seq 0 0`; do
         for i in `seq 0 $count`; do
             #echo $i
             cname=`echo $awsout | jq ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}" | tr -d '"'`
-            echo $cname
-            fn=`printf "%s__%s.tf" $ttft $cname`
+            rname=${cname//:/_}
+            rname=${rname//\//_}
+            echo "cname=$cname"
+            
+            fn=`printf "%s__%s.tf" $ttft $rname`
             if [ -f "$fn" ] ; then
                 echo "$fn exists already skipping"
                 continue
             fi
-            printf "resource \"%s\" \"%s\" {" $ttft $cname > $ttft.$cname.tf
-            printf "}" $cname >> $ttft.$cname.tf
-            printf "terraform import %s.%s %s" $ttft $cname $cname > import_$ttft_$cname.sh
-            terraform import $ttft.$cname $cname
-            terraform state show $ttft.$cname > t2.txt
-            tfa=`printf "%s.%s" $ttft $cname`
+            printf "resource \"%s\" \"%s\" {" $ttft $rname > $fn
+            printf "}" $cname >> $fn
+            printf "terraform import %s.%s %s" $ttft $rname $cname > import_$ttft_$rname.sh
+            terraform import $ttft.$rname $cname
+            terraform state show $ttft.$rname > t2.txt
+            tfa=`printf "%s.%s" $ttft $rname`
             terraform show  -json | jq --arg myt "$tfa" '.values.root_module.resources[] | select(.address==$myt)' > $tfa.json
             #echo $awsj | jq . 
-            rm $ttft.$cname.tf
+            rm $ttft.$rname.tf
             cat t2.txt | perl -pe 's/\x1b.*?[mGKH]//g' > t1.txt
             #	for k in `cat t1.txt`; do
             #		echo $k
@@ -57,7 +60,8 @@ for c in `seq 0 0`; do
                     if [[ ${tt1} == "role_arn" ]];then skip=1;fi
                     if [[ ${tt1} == "owner_id" ]];then skip=1;fi
                     if [[ ${tt1} == "ipv6_cidr_block_association_id" ]];then skip=1;fi
-                    #if [[ ${tt1} == "availability_zone" ]];then skip=1;fi
+                    if [[ ${tt1} == "registry_id" ]];then skip=1;fi
+                    if [[ ${tt1} == "repository_url" ]];then skip=1;fi                   
                     if [[ ${tt1} == "availability_zone_id" ]];then skip=1;fi
                     if [[ ${tt1} == "vpc_id" ]]; then
                         tt2=`echo $tt2 | tr -d '"'`
@@ -72,15 +76,15 @@ for c in `seq 0 0`; do
                 
             done <"$file"
 
-            ofn=`printf "output__%s__%s.tf" $ttft $cname`
+            ofn=`printf "output__%s__%s.tf" $ttft $rname`
             printf "output \"%s__%s__id\" {\n" $ttft $cname > $ofn
-            printf "value = %s.%s.id\n" $ttft $cname >> $ofn
-            printf "}\n" $ttft $cname >> $ofn
+            printf "value = %s.%s.name\n" $ttft $rname >> $ofn
+            printf "}\n" >> $ofn
 
-            dfn=`printf "data/data_%s__%s.tf" $ttft $cname`
-            printf "data \"%s\" \"%s\" {\n" $ttft $cname > $dfn
-            printf "id = \"%s\"\n" $cname >> $dfn
-            printf "}\n" $ttft $cname >> $dfn
+            dfn=`printf "data/data_%s__%s.tf" $ttft $rname`
+            printf "data \"%s\" \"%s\" {\n" $ttft $rname > $dfn
+            printf "name = \"%s\"\n" $cname >> $dfn
+            printf "}\n" >> $dfn
             
         done
         terraform fmt
